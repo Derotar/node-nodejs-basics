468c671d941e0a39c97405b036455d1a8e45af90
EgorGotbit <egor.nikolaev@gotbit.io>
2024-10-08 02:56:55 +0300

    feat: implement child_process logic

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,28 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { fork } from "node:child_process";
+
+const filePath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "files",
+  "script.js"
+);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(filePath, args, { stdio: "pipe" });
+
+  process.stdin.on("data", (chunk) => {
+    const chunkStr = chunk.toString();
+    child.stdin.write(chunkStr);
+  });
+
+  child.stdout.on("data", (chunk) => {
+    const chunkStr = chunk.toString();
+    process.stdout.write(chunkStr);
+  });
+
+  child.on("exit", (code) => process.exit(code));
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3]);
