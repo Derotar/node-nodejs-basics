e90862625139daec587073f9fda1f041b8a3123d
EgorGotbit <egor.nikolaev@gotbit.io>
2024-10-07 22:05:46 +0300

    feat:implement calcHash.js logic

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createHash } from "node:crypto";
+import * as fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const filePath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "files",
+  "fileToCalculateHashFor.txt"
+);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+
+  const readStream = fs.createReadStream(filePath, { highWaterMark: 2 });
+
+  readStream.on("data", (chunk) => {
+    hash.update(chunk.toString());
+  });
+
+  readStream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
