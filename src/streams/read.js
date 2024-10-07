6a42636cdf2b276ad60381781261bccc88daa1d6
EgorGotbit <egor.nikolaev@gotbit.io>
2024-10-07 21:26:50 +0300

    feat: implement read stream logic

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import * as fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const filePath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "files",
+  "fileToRead.txt"
+);
+
 const read = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(filePath);
+  readStream.pipe(process.stdout);
 };
 
 await read();
