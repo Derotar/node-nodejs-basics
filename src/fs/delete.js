85f44991931796380b86db25ad2609aa9fdf5e0d
EgorGotbit <egor.nikolaev@gotbit.io>
2024-10-07 04:18:20 +0300

    feat: implement delete file logic

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import * as fsPromises from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const fileToRemovePath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "files",
+  "fileToRemove.txt"
+);
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fsPromises.rm(fileToRemovePath);
+  } catch (e) {
+    if (e.code !== "ENOENT") throw e;
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
