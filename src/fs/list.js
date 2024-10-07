8a241365e2e620fcc81df073235c4261f7b5085c
EgorGotbit <egor.nikolaev@gotbit.io>
2024-10-07 04:21:36 +0300

    feat: implement list files logic

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import * as fsPromises from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const folderPath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "files"
+);
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fsPromises.readdir(folderPath);
+    console.log(files);
+  } catch (e) {
+    if (e.code !== "ENOENT") {
+      throw e;
+    }
+
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
